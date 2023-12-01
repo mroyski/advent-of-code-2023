fedcc11620db899e876b4a297c0867446c74382d
Michael Royski <mroyski@gmail.com>
2023-12-01 17:44:25 -0500

    day 2 part 2

--- 01/01.js
+++ 01/01.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-// Part 1
 let input = fs.readFileSync('input.txt', (err, data) => {
   if (err) {
     throw err;
@@ -11,21 +10,10 @@ let input = fs.readFileSync('input.txt', (err, data) => {
 
 input = input.toString().split('\n');
 
-const keys = {
-  zero: '0',
-  one: '1',
-  two: '2',
-  three: '3',
-  four: '4',
-  five: '5',
-  six: '6',
-  seven: '7',
-  eight: '8',
-  nine: '9',
-};
 
+
+// Part 1
 let total = 0;
-
 input.forEach((line) => {
   const digitsOnly = /\d/g;
   const digits = line.match(digitsOnly);
@@ -39,6 +27,70 @@ input.forEach((line) => {
   total += result;
 });
 
-console.log(total);
+console.log('Part1: ', total);
 
 // 55130
+
+
+
+// Part 2
+total = 0;
+const keys = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
+input.forEach((line) => {
+  let firstDigit = null;
+  let secondDigit = null;
+
+  // get first digit
+  for (let i = 0; i < line.length; i++) {
+    let subString = line.substring(i, line.length);
+    console.log(subString);
+
+    if (subString[0].match(/[0-9]/)) {
+      firstDigit = subString[0];
+    } else {
+      for (k in keys) {
+        if (subString.startsWith(k)) {
+          firstDigit = keys[k];
+        }
+      }
+    }
+
+    if (firstDigit !== null) break;
+  }
+
+  // get second digit
+  for (let i = line.length - 1; i >= 0; i--) {
+    let subString = line.substring(i, line.length);
+
+    if (subString[0].match(/[0-9]/)) {
+      secondDigit = subString[0];
+      break;
+    }
+
+    for (k in keys) {
+      if (subString.startsWith(k)) {
+        secondDigit = keys[k];
+        break;
+      }
+    }
+
+    if (secondDigit !== null) break;
+  }
+
+  result = parseInt(`${firstDigit}${secondDigit}`);
+  console.log(result);
+  total += result;
+});
+
+// 54985
