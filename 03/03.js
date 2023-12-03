ed1c1b399f84d3d468595435db77bb3e4980c1e3
Michael Royski <mroyski@gmail.com>
2023-12-03 15:40:04 -0500

    day3 part2

--- 03/03.js
+++ 03/03.js
@@ -18,69 +18,173 @@ const isSymbol = (char) => {
   return char.match(/[^.\d]/);
 };
 
-const symbolAdjacent = (y, x) => {
-    console.log(y, x)
-  // left
-  if (x > 0) {
-    if (isSymbol(input[y][x - 1])) return true;
-  }
-
-  // right
-  if (x < input[y].length - 1) {
-    if (isSymbol(input[y][x + 1])) return true;
-  }
-
-  // top
-  if (y > 0) {
+const part1 = () => {
+  const symbolAdjacent = (y, x) => {
     // left
     if (x > 0) {
-      if (isSymbol(input[y - 1][x - 1])) return true;
+      if (isSymbol(input[y][x - 1])) return true;
     }
-    // center
-    if (isSymbol(input[y - 1][x])) return true;
-    // right
-    if (x < input[y].length - 1) {
-      if (isSymbol(input[y - 1][x + 1])) return true;
-    }
-  }
 
-  // bottom
-  if (y < input.length - 1) {
-    // left
-    if (x > 0) {
-      if (isSymbol(input[y + 1][x - 1])) return true;
-    }
-    // center
-    if (isSymbol(input[y + 1][x])) return true;
     // right
     if (x < input[y].length - 1) {
-      if (isSymbol(input[y + 1][x + 1])) return true;
+      if (isSymbol(input[y][x + 1])) return true;
+    }
+
+    // top
+    if (y > 0) {
+      // left
+      if (x > 0) {
+        if (isSymbol(input[y - 1][x - 1])) return true;
+      }
+      // center
+      if (isSymbol(input[y - 1][x])) return true;
+      // right
+      if (x < input[y].length - 1) {
+        if (isSymbol(input[y - 1][x + 1])) return true;
+      }
+    }
+
+    // bottom
+    if (y < input.length - 1) {
+      // left
+      if (x > 0) {
+        if (isSymbol(input[y + 1][x - 1])) return true;
+      }
+      // center
+      if (isSymbol(input[y + 1][x])) return true;
+      // right
+      if (x < input[y].length - 1) {
+        if (isSymbol(input[y + 1][x + 1])) return true;
+      }
+    }
+    return false;
+  };
+
+  let sum = 0;
+  let number = '';
+  let adjacent = false;
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      if (isNumber(input[y][x])) {
+        number = number.concat(input[y][x]);
+        if (symbolAdjacent(y, x)) {
+          adjacent = true;
+        }
+      } else {
+        if (number !== '' && adjacent) {
+          sum += parseInt(number);
+        }
+        number = '';
+        adjacent = false;
+      }
     }
   }
-  return false;
+  console.log(sum);
 };
-
 let sum = 0;
 let number = '';
+let tempSymbols = {};
+let symbols = {};
 let adjacent = false;
 
+const part2 = () => {
+  const symbolAdjacent = (y, x) => {
+    // left
+    if (x > 0) {
+      if (isSymbol(input[y][x - 1])) {
+        tempSymbols[`${y},${x - 1}`] = true;
+        return true;
+      }
+    }
 
-for (let y = 0; y < input.length; y++) {
-  for (let x = 0; x < input[y].length; x++) {
-    if (isNumber(input[y][x])) {
-      number = number.concat(input[y][x]);
-      if (symbolAdjacent(y, x)) {
-        adjacent = true;
+    // right
+    if (x < input[y].length - 1) {
+      if (isSymbol(input[y][x + 1])) {
+        tempSymbols[`${y},${x + 1}`] = true;
+        return true;
       }
-    } else {
-      if (number !== '' && adjacent) {
-        console.log(number)
-        sum += parseInt(number);
+    }
+
+    // top
+    if (y > 0) {
+      // left
+      if (x > 0) {
+        if (isSymbol(input[y - 1][x - 1])) {
+          tempSymbols[`${y - 1},${x - 1}`] = true;
+          return true;
+        }
       }
-      number = '';
-      adjacent = false;
+      // center
+      if (isSymbol(input[y - 1][x])) {
+        tempSymbols[`${y - 1},${x}`] = true;
+        return true;
+      }
+      // right
+      if (x < input[y].length - 1) {
+        if (isSymbol(input[y - 1][x + 1])) {
+          tempSymbols[`${y - 1},${x + 1}`] = true;
+          return true;
+        }
+      }
+    }
+
+    // bottom
+    if (y < input.length - 1) {
+      // left
+      if (x > 0) {
+        if (isSymbol(input[y + 1][x - 1])) {
+          tempSymbols[`${y + 1},${x - 1}`] = true;
+          return true;
+        }
+      }
+      // center
+      if (isSymbol(input[y + 1][x])) {
+        tempSymbols[`${y + 1},${x}`] = true;
+        return true;
+      }
+      // right
+      if (x < input[y].length - 1) {
+        if (isSymbol(input[y + 1][x + 1])) {
+          tempSymbols[`${y + 1},${x + 1}`] = true;
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      if (isNumber(input[y][x])) {
+        number = number.concat(input[y][x]);
+        if (symbolAdjacent(y, x)) {
+          adjacent = true;
+        }
+      } else {
+        if (number !== '' && adjacent) {
+          for (k in tempSymbols) {
+            if (symbols[k]) {
+              symbols[k].push(parseInt(number));
+            } else {
+              symbols[k] = [parseInt(number)];
+            }
+          }
+        }
+        number = '';
+        adjacent = false;
+        tempSymbols = {};
+      }
     }
   }
-}
 
-console.log(sum)
+  for (s in symbols) {
+    if (symbols[s].length === 2) {
+      sum += symbols[s][0] * symbols[s][1];
+    }
+  }
+
+  console.log(sum)
+};
+
+part1(); // 550934
+part2(); // 81997870
