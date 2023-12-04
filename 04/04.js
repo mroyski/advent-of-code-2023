c23feb199ee8080bf4c28af2e7743fa7fb7d59a2
Michael Royski <mroyski@gmail.com>
2023-12-04 17:22:48 -0500

    day4 part2

--- 04/04.js
+++ 04/04.js
@@ -10,40 +10,88 @@ let input = fs.readFileSync(__dirname + '/input.txt', (err, data) => {
 
 input = input.toString().split('\n');
 
-console.log(input);
+const part1 = () => {
+  let sum = 0;
 
-let sum = 0;
+  input.forEach((line) => {
+    const winningNumbers = line
+      .split('|')[0]
+      .split(':')[1]
+      .split(' ')
+      .filter((s) => s);
+    const cardNumbers = line
+      .split('|')[1]
+      .split(' ')
+      .filter((s) => s);
 
-input.forEach((line) => {
-  const winningNumbers = line
-    .split('|')[0]
-    .split(':')[1]
-    .split(' ')
-    .filter((s) => s);
-  const cardNumbers = line
-    .split('|')[1]
-    .split(' ')
-    .filter((s) => s);
+    const winners = {};
+    winningNumbers.forEach((number) => {
+      winners[number] = true;
+    });
 
-  const winners = {};
-  winningNumbers.forEach((number) => {
-    winners[number] = true;
+    let points = 0;
+    cardNumbers.forEach((number) => {
+      if (winners[number]) {
+        if (points === 0) {
+          points = 1;
+        } else {
+          points *= 2;
+        }
+      }
+    });
+
+    sum += points;
   });
 
-  let points = 0;
-  cardNumbers.forEach((number) => {
-    if (winners[number]) {
-      console.log('number', number);
+  console.log(sum);
+};
 
-      if (points === 0) {
-        points = 1;
-      } else {
-        points *= 2;
+const part2 = () => {
+  let cards = {};
+
+  for (let i = 0; i < input.length; i++) {
+    const currentNumber = i + 1;
+
+    const winningNumbers = input[i]
+      .split('|')[0]
+      .split(':')[1]
+      .split(' ')
+      .filter((s) => s);
+    const cardNumbers = input[i]
+      .split('|')[1]
+      .split(' ')
+      .filter((s) => s);
+
+    let matchCount = 0;
+    cardNumbers.forEach((number) => {
+      if (winningNumbers.includes(number)) {
+        matchCount += 1;
+      }
+    });
+
+    cards[currentNumber] = {
+      number: currentNumber,
+      matches: matchCount,
+      count: 1,
+    };
+  }
+
+  for (let i = 1; i <= input.length; i++) {
+    let matches = cards[i]['matches'];
+
+    for (let j = matches; j > 0; j--) {
+      if (cards[i + j]) {
+        cards[i + j]['count'] += cards[i]['count'];
       }
     }
-  });
+  }
 
-  sum += points;
-});
+  let sum = 0;
+  for (c in cards) {
+    sum += cards[c]['count'];
+  }
+  console.log(sum);
+};
 
-console.log(sum); // 26346
+part1(); // 26346
+part2(); // 8467762
