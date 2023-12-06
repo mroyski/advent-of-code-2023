30d3dc6593bf8701b97a08a9e353d4186f596525
Michael Royski <mroyski@gmail.com>
2023-12-06 10:38:55 -0500

    day6 part2

--- 06/06.js
+++ 06/06.js
@@ -9,35 +9,70 @@ let input = fs.readFileSync(__dirname + '/input.txt', (err, data) => {
 });
 
 input = input.toString().split('\n');
-const times = input[0]
-  .split(':')[1]
-  .split(' ')
-  .filter((t) => t);
-const distances = input[1]
-  .split(':')[1]
-  .split(' ')
-  .filter((d) => d);
 
-console.log(times);
-console.log(distances);
+const part1 = () => {
+  const times = input[0]
+    .split(':')[1]
+    .split(' ')
+    .filter((t) => t);
+  const distances = input[1]
+    .split(':')[1]
+    .split(' ')
+    .filter((d) => d);
 
-let sum = 1;
+  console.log(times);
+  console.log(distances);
 
-for (let i = 0; i < times.length; i++) {
-  const time = parseInt(times[i]);
-  const distance = parseInt(distances[i]);
+  let sum = 1;
+
+  for (let i = 0; i < times.length; i++) {
+    const time = parseInt(times[i]);
+    const distance = parseInt(distances[i]);
+
+    let wins = 0;
+
+    for (let hold = 1; hold <= time; hold++) {
+      let travelled = (time - hold) * hold;
+      if (travelled > distance) {
+        wins += 1;
+      }
+    }
+
+    sum *= wins;
+  }
+
+  console.log(sum);
+};
+
+const part2 = () => {
+  const time = parseInt(
+    input[0]
+      .split(':')[1]
+      .split(' ')
+      .filter((t) => t)
+      .join('')
+  );
+  const distance = parseInt(
+    input[1]
+      .split(':')[1]
+      .split(' ')
+      .filter((d) => d)
+      .join('')
+  );
+
+  console.log(time);
+  console.log(distance);
 
   let wins = 0;
 
-  for (let hold = 1; hold <= time; hold++) {
+  for (let hold = 1; hold < time; hold++) {
     let travelled = (time - hold) * hold;
     if (travelled > distance) {
       wins += 1;
     }
   }
+  console.log('wins: ', wins);
+};
 
-  console.log('wins: ', wins)
-  sum *= wins;
-}
-
-console.log(sum); // 2612736
+part1(); // 2612736
+part2(); // 29891250
