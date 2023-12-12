88492ab84286239859c8197929dd2083506d7a4e
Michael Royski <mroyski@gmail.com>
2023-12-11 22:26:08 -0500

    day10 part2

--- 10/10.js
+++ 10/10.js
@@ -37,34 +37,47 @@ for (let i = 0; i < input.length; i++) {
   }
 }
 
-let sum = 0;
+const solution = (modifier) => {
+  let sum = 0;
 
-for (let i = 0; i < galaxies.length - 1; i++) {
-  let left = galaxies[i];
+  for (let i = 0; i < galaxies.length - 1; i++) {
+    let left = galaxies[i];
 
-  for (let j = i + 1; j < galaxies.length; j++) {
-    let right = galaxies[j];
+    for (let j = i + 1; j < galaxies.length; j++) {
+      let right = galaxies[j];
 
-    let rowDiff = Math.abs(left[0] - right[0]);
-    let colDiff = Math.abs(left[1] - right[1]);
+      let rowDiff = Math.abs(left[0] - right[0]);
+      let colDiff = Math.abs(left[1] - right[1]);
 
-    let colModifiers = emptyColumns.filter((r) => {
-      if (left[1] < right[1]) return r > left[1] && r < right[1];
-      else return r < left[1] && r > right[1];
-    });
+      let colModifiers = emptyColumns.filter((c) => {
+        if (left[1] < right[1]) return c > left[1] && c < right[1];
+        else return c < left[1] && c > right[1];
+      });
 
-    let rowModifiers = emptyRows.filter((c) => {
-      if (left[0] < right[0]) return c > left[0] && c < right[0];
-      else return c < left[0] && c > right[0];
-    });
+      let rowModifiers = emptyRows.filter((r) => {
+        if (left[0] < right[0]) return r > left[0] && r < right[0];
+        else return r < left[0] && r > right[0];
+      });
 
-    rowDiff += rowModifiers.length;
-    colDiff += colModifiers.length;
+      rowDiff += rowModifiers.length * modifier;
+      colDiff += colModifiers.length * modifier;
 
-    let distance = colDiff + rowDiff;
+      if (modifier > 1) {
+        rowDiff -= rowModifiers.length;
+        colDiff -= colModifiers.length;
+      }
 
-    sum += distance;
+      let distance = colDiff + rowDiff;
+
+      sum += distance;
+    }
   }
-}
 
-console.log('sum:', sum); // 10292708
+  console.log('sum:', sum); // 10292708
+};
+
+// Part 1
+solution(1); // 10292708
+
+// Part 2
+solution(1000000); // 790194712336
